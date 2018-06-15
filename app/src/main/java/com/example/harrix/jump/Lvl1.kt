bfde5ca837eb440e4cc8f4d6ba23e38780a48716
Alina <alya_ush@mail.ru>
2018-06-15 21:07:55 +0300

    3 commit

---
+++ app/src/main/java/com/example/harrix/jump/Lvl1.kt
@@ -0,0 +1,15 @@
+package com.example.harrix.jump
+
+//import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_lvl1.*
+
+class Lvl1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lvl1)
+
+    }
+}
